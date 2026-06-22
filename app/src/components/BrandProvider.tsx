9b65292e29a51c1d6465eed36784300902e4fbd5
shaneweisz <shaneweisz@gmail.com>
2026-06-22 20:51:12 +0100

    Update brand title to "Dash of Life: A Dashboard for Species Conservation".

--- app/src/components/BrandProvider.tsx
+++ app/src/components/BrandProvider.tsx
@@ -4,7 +4,7 @@ import { createContext, useContext } from "react";
 import type { Brand } from "../config/brand";
 
 const BrandContext = createContext<Brand>({
-  title: "Dash of Life: A Dashboard of Earth's Species",
+  title: "Dash of Life: A Dashboard for Species Conservation",
   description: "A dashboard for biodiversity data about life on Earth",
   assessedTabLabel: "Red List Assessed",
   unassessedTabLabel: "Unassessed",
